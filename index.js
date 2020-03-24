96a5cea6b7c2591ed194c5699e5d287d9883bd04
unknown <alexis.platino@gmail.com>
2020-03-24 21:07:52 +0100

    I thought i fixed this already wtf?

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const {Pinger, utils} = require('./src/pinger');
 
-(async () => {
-    const dataUtils = await utils.getDetails();
-    const x = new Pinger(null, dataUtils.address);
-    const data = await x.ping("192.168.1.33");
-    console.log(data);
+module.exports = (() => {
+    const pinger = new Pinger();
+    return {pinger, utils}
 })();
-
-module.exports = {Pinger, utils};
